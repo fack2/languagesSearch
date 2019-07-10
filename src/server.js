bfe56ae5d8efa0707caa716e61be3f361721795e
yaqootturman <yaqoot.turman@gmail.com>
2019-07-10 14:09:58 +0300

    port

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const http = require("http");
 const router = require("./router");
-
+const PORT = process.env.PORT || 4000;
 const server = http.createServer(router);
 
-server.listen(process.env.PORT || 4000, () => {
+server.listen(PORT, () => {
   console.log(
     "Server is listening on http://localhost:000. Ready to accept requests!"
   );
