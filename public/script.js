89b0c1c0a996ed818b805655249983c17235f0a1
Rand Inaim <rand.inaim95@gmail.com>
2019-07-10 11:09:40 +0300

    css styles

--- public/script.js
+++ public/script.js
@@ -1,31 +1,32 @@
 function request(url, cb) {
-  fetch(url)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      return cb(data);
-    });
+	fetch(url)
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			return cb(data);
+		});
 }
-const searchBox = document.getElementById("searchBox");
-searchBox.addEventListener("keyup", () => {
-  var inputValue = searchBox.value;
-  request(`/search=${inputValue}`, lang => {
-    console.log(lang);
+const searchBox = document.getElementById('searchBox');
+searchBox.addEventListener('keyup', () => {
+	var inputValue = searchBox.value;
+	request(`/search=${inputValue}`, (lang) => {
+		console.log(lang);
 
-    const langList = document.getElementById("langList");
-    langList.innerHTML = "";
+		const langList = document.getElementById('langList');
+		langList.innerHTML = '';
+		langList.style.display = 'none';
+		lang.forEach((element) => {
+			langList.style.display = 'block';
 
-    lang.forEach(element => {
-      const langItem = document.createElement("a");
-      const br = document.createElement("br");
-      langItem.setAttribute("href", "#");
-      langItem.innerHTML = element.name;
-
-      langList.appendChild(langItem);
-      langList.appendChild(br);
-
-      document.body.appendChild(langList);
-    });
-  });
+			const langItem = document.createElement('a');
+			langItem.setAttribute('class', 'item');
+			const br = document.createElement('br');
+			const form = document.getElementById('form');
+			langItem.setAttribute('href', '#');
+			langItem.innerHTML = element.name;
+			langList.appendChild(langItem);
+			langList.appendChild(br);
+		});
+	});
 });
--- src/handler.js
+++ src/handler.js
@@ -1,67 +1,65 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const homeHandler = (request, response) => {
-  const filePath = path.join(__dirname, "/../public/index.html");
-  const html = fs.readFile(filePath, (error, file) => {
-    if (error) {
-      response.writeHead(500, { "Content-Type": "text/html" });
-      response.end("<h1>server Error</h1>");
-    } else {
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.end(file);
-    }
-  });
+	const filePath = path.join(__dirname, '/../public/index.html');
+	const html = fs.readFile(filePath, (error, file) => {
+		if (error) {
+			response.writeHead(500, { 'Content-Type': 'text/html' });
+			response.end('<h1>server Error</h1>');
+		} else {
+			response.writeHead(200, { 'Content-Type': 'text/html' });
+			response.end(file);
+		}
+	});
 };
 
 const searchHandler = (request, response, url) => {
-  let searchLang = url.split("=")[1];
-  const filePath = path.join(__dirname, "./", "data/db.json");
-  fs.readFile(filePath, "utf-8", (err, file) => {
-    if (err) console.log(err);
-    else if(searchLang.length>0){
-      response.writeHead(200, { "Content-Type": "application/json" });
-      let allLang = JSON.parse(file);
-      const filterLang = [];
-  const firstLetter = searchLang[0].toUpperCase();
-  searchLang = firstLetter + searchLang.slice(1);
-  allLang.forEach(element => {
-    if (element.name.indexOf(searchLang) === 0) {
-      filterLang.push(element);
-    }
-  }); 
-      response.end(JSON.stringify(filterLang));
-    }
-    else{
-      response.writeHead(200, { "Content-Type": "application/json" });
-      response.end(JSON.stringify(""));
-
-    }
-  });
-};  
+	let searchLang = url.split('=')[1];
+	const filePath = path.join(__dirname, './', 'data/db.json');
+	fs.readFile(filePath, 'utf-8', (err, file) => {
+		if (err) console.log(err);
+		else if (searchLang.length > 0) {
+			response.writeHead(200, { 'Content-Type': 'application/json' });
+			let allLang = JSON.parse(file);
+			const filterLang = [];
+			const firstLetter = searchLang[0].toUpperCase();
+			searchLang = firstLetter + searchLang.slice(1);
+			allLang.forEach((element) => {
+				if (element.name.indexOf(searchLang) === 0) {
+					filterLang.push(element);
+				}
+			});
+			response.end(JSON.stringify(filterLang));
+		} else {
+			response.writeHead(200, { 'Content-Type': 'application/json' });
+			response.end(JSON.stringify(''));
+		}
+	});
+};
 
 const publicHandler = (request, response, url) => {
-  const extension = url.split(".")[1];
-  const extensionType = {
-    html: "text/html",
-    css: "text/css",
-    js: "application/javascript",
-    jpg: "image/jpg",
-    png: "image/png",
-    ico: "image/x-icon"
-  };
-  const filePath = path.join(__dirname, "../public", url);
-  fs.readFile(filePath, (error, file) => {
-    if (error) {
-      response.writeHead(500, { "Content-Type": "text/html" });
-      response.end("<h1>this is error message should be</h1>");
-    } else {
-      console.log("extensionType", extension);
+	const extension = url.split('.')[1];
+	const extensionType = {
+		html: 'text/html',
+		css: 'text/css',
+		js: 'application/javascript',
+		jpg: 'image/jpg',
+		png: 'image/png',
+		ico: 'image/x-icon'
+	};
+	const filePath = path.join(__dirname, '../public', url);
+	fs.readFile(filePath, (error, file) => {
+		if (error) {
+			response.writeHead(500, { 'Content-Type': 'text/html' });
+			response.end('<h1>this is error message should be</h1>');
+		} else {
+			console.log('extensionType', extension);
 
-      response.writeHead(200, { "Content-Type": extensionType[extension] });
-      response.end(file);
-    }
-  });
+			response.writeHead(200, { 'Content-Type': extensionType[extension] });
+			response.end(file);
+		}
+	});
 };
 
 module.exports = { homeHandler, searchHandler, publicHandler };
