0b8339fdf6a371fc73d663f288d0365342118f1d
YousefQwasmeh <151037@ppu.edu.ps>
2019-07-10 14:56:44 +0300

    more

--- public/script.js
+++ public/script.js
@@ -15,6 +15,28 @@ searchButton.addEventListener("click",()=>{
 	// })	
 	window.location.assign(`https://www.google.com/search?q=${searchBox.value}`)
 })
+let lan="ar";
+function ran(){
+	// request("https://baconipsum.com/api/?type=meat-and-filler", (aaa) => {
+
+	request("http://www.randomtext.me/api/lorem/ul-5/5-15", (aaa) => {
+		console.log("5555555555555");	
+		const randomText=document.getElementById("randomText");
+		randomText.innerHTML=aaa.text_out;
+				console.log(randomText.innerText);	
+				const key = 'trnsl.1.1.20190702T084028Z.6d9a7275087030e7.c7f5e34f73df150db59b46361478dc0c93bfd86c';
+		request(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${key}&text=${randomText.innerText}&lang=la-${lan}`, (bbb) => {
+			randomText.innerText=bbb.text[0];
+		})
+		// aaa.forEach(e=>{
+		// 	randomText.innerHTML+=e+"<br>";
+
+		// })
+		// console.log(aaa);
+		
+	})	
+	// window.location.assign(`https://www.google.com/search?q=${searchBox.value}`)
+}
 // const a=document.querySelectorAll("a");
 
 // a.addEventListener("click",()=>{
@@ -38,6 +60,8 @@ searchBox.addEventListener('keyup', () => {
 
 			const langItem = document.createElement('a');
 			langItem.addEventListener("click",()=>{
+				lan=element.code;
+				ran();
 				searchBox.value=langItem.innerText;
 		langList.style.display = 'none';
 
