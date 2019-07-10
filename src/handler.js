0e664fe77ebf033cf0e22da163cef360d0d83dff
YousefQwasmeh <151037@ppu.edu.ps>
2019-07-10 08:50:50 +0300

    solve upperCase & blank issues

--- src/handler.js
+++ src/handler.js
@@ -15,35 +15,31 @@ const homeHandler = (request, response) => {
 };
 
 const searchHandler = (request, response, url) => {
-  const s = url.split("=")[1];
+  let searchLang = url.split("=")[1];
   const filePath = path.join(__dirname, "./", "data/db.json");
   fs.readFile(filePath, "utf-8", (err, file) => {
     if (err) console.log(err);
-    else {
+    else if(searchLang.length>0){
       response.writeHead(200, { "Content-Type": "application/json" });
-      let arr = JSON.parse(file);
-      const ar = [];
-      s.toUpperCase();
-      arr.forEach(element => {
-        // console.l og(element.name);
+      let allLang = JSON.parse(file);
+      const filterLang = [];
+  const firstLetter = searchLang[0].toUpperCase();
+  searchLang = firstLetter + searchLang.slice(1);
+  allLang.forEach(element => {
+    if (element.name.indexOf(searchLang) === 0) {
+      filterLang.push(element);
+    }
+  }); 
+      response.end(JSON.stringify(filterLang));
+    }
+    else{
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.end(JSON.stringify(""));
 
-        if (element.name.indexOf(s) !== -1) {
-          ar.push(element);
-        }
-
-        // ar.push(element);
-      });
-      //   console.log("000000000000000000000000000000000");
-      //   const jso = arr.filter(x => {
-      //     console.log(x.name == "Arabic");
-      //     x.name == "Arabic";
-      //   });
-      // console.log(ar);
-
-      response.end(JSON.stringify(ar));
     }
   });
-};
+};  
+
 const publicHandler = (request, response, url) => {
   const extension = url.split(".")[1];
   const extensionType = {
